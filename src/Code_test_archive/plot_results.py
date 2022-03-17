52151c6abb9d3e46890b2ab5987ec01a703f452f
ksjeong99 <ksjeong99@gmail.com>
2022-03-17 13:51:05 -0600

    update_plot.py

--- src/Code_test_archive/plot_results.py
+++ src/Code_test_archive/plot_results.py
@@ -1,13 +1,16 @@
 # %%
+from matplotlib.image import AxesImage
 import pandas as pd
 import numpy as np
 #import geopandas as gpd
 #import networkx as nx
 import matplotlib.pyplot as plt
 from argparse import ArgumentParser
+import seaborn as sns
 #import osmnx as ox
 #import plotly.graph_objects as go
 
+# %%
 parser = ArgumentParser()
 parser.add_argument("-st", "--shipment type", dest="ship_type",
                     help="B2B or B2C", required=True, type=str)
@@ -32,3 +35,37 @@ for count_num in county_list:
     ax.set_ylim(BBox[2],BBox[3])
     ax.imshow(sf_map, zorder=0, extent = BBox, aspect= 'equal')
     fig.savefig('../../../FRISM_input_output/Sim_outputs/{0}payload_plot_county{1}.png'.format(ship_type, count_num))
+
+################################################################################################################
+# %%
+
+study_region="AT"
+web_obs=pd.read_csv('../../../FRISM_input_output_{}/Sim_outputs/Generation/{}_webuse_by_income_observed.csv'.format(study_region,study_region))
+online_bi_obs=pd.read_csv('../../../FRISM_input_output_{}/Sim_outputs/Generation/{}_online_by_income_observed.csv'.format(study_region,study_region))
+
+df_hh_obs=pd.read_csv('../../../FRISM_input_output_{}/Model_inputs/NHTS/{}_hh.csv'.format(study_region,study_region))
+df_per_obs=pd.read_csv('../../../FRISM_input_output_{}/Model_inputs/NHTS/{}_per.csv'.format(study_region,study_region))
+df_per_obs_hh=df_per_obs.groupby(['HOUSEID'])['DELIVER'].agg(delivery_f='sum').reset_index()
+df_hh_obs=df_hh_obs.merge(df_per_obs_hh, on='HOUSEID', how='left')
+
+df_hh_model=pd.read_csv('../../../FRISM_input_output_{}/Sim_outputs/Generation/households_del.csv'.format(study_region))
+
+# %%
+df_hh_obs['delivery_f'] =df_hh_obs['delivery_f'].apply(lambda x: 0 if np.isnan(x) else int(x))
+df_hh_model['delivery_f'] =df_hh_model['delivery_f'].apply(lambda x: 1 if np.isnan(x) else int(x))
+
+# %%
+list_income=["income_cls_0","income_cls_1","income_cls_2","income_cls_3"]
+dic_income={"income_cls_0": "income <$35k",
+            "income_cls_1": "income $35k-$75k",
+            "income_cls_2": "income $75k-125k",
+            "income_cls_3": "income >$125k"}
+     
+for ic_nm in list_income:
+    plt.figure(figsize = (8,6))
+    plt.hist(df_hh_obs[df_hh_obs[ic_nm]==1]['delivery_f'], color ="blue", density=True, bins=30, alpha = 0.3, label="observed")
+    plt.hist(df_hh_model[(df_hh_model[ic_nm]==1) & (df_hh_model['delivery_f']<=30)]['delivery_f'], color ="red", density=True, bins=30, alpha = 0.3, label="modeled")
+    plt.title("Density of Delivery Frequency in {0}, {1}".format(dic_income[ic_nm], study_region))
+    plt.legend(loc="upper right")
+    plt.savefig('../../../FRISM_input_output_{0}/Sim_outputs/Generation/B2C_delivery_val_{1}.png'.format(study_region, ic_nm))
+# %%
