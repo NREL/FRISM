61bfc23152ab73d5ac600d4137aa921059d440f5
ksjeong99 <ksjeong99@gmail.com>
2022-04-08 12:29:34 -0600

    remove a zone

--- src/Simulation/config.py
+++ src/Simulation/config.py
@@ -125,7 +125,7 @@ CBG_file= 'sfbay_freight.geojson'
 ##ship_direction = 'out' # ['out','in', 'all']
 commodity_list= ["1", "2", "3", "4", "5"]
 county_list=[1, 13, 41, 55, 75, 81, 85, 95, 97] ## this should be updated 
-list_error_zone=[1047.0, 1959.0, 1979.0, 2824.0, 3801.0, 3897.0, 4303.0, 6252.0, 6810.0, 7273.0, 8857.0, 9702.0, 6248.0] # this should be updated
+list_error_zone=[1047.0, 1959.0, 1979.0, 2824.0, 3801.0, 3897.0, 4303.0, 6252.0, 6810.0, 7273.0, 8857.0, 9702.0, 20028.0, 6248.0] # this should be updated
 weight_theshold=40000
 md_cap =11000
 hd_cap=40000
@@ -143,6 +143,7 @@ fdir_main_output= "../../../FRISM_input_output_{}/Sim_outputs/Shipment2Fleet/".f
 Farallon island
 MESOZONE = 6248
 GEOID = 060759804011
+Hawaii = 20028
 '''
 
 # County: [1, 13, 41, 55, 75, 81, 85, 95, 97], all = 9999
