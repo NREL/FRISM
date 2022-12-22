4e310fc45152c25bab6c92d4c9483689fe89cc41
ksjeong99 <ksjeong99@gmail.com>
2022-12-22 09:03:33 -0700

    update ship file with new generation always

--- src/Simulation_AT/Shipment2Fleet_veh_tech.py
+++ src/Simulation_AT/Shipment2Fleet_veh_tech.py
@@ -1155,52 +1155,52 @@ def b2b_d_select_with_ship_size(TruckLoad,w_th, b2b_day_factor):
 
 def b2b_d_shipment_by_commodity(fdir,commoidty, weight_theshold, CBGzone_df,sel_county,ship_direction, county_wo_sel, b2b_day_factor):
     daily_b2b_fname=fdir+'Daily_sctg%s_OD_%s_%s.csv' % (commoidty, sel_county,ship_direction)
-    if file_exists(daily_b2b_fname):
-        B2BF=pd.read_csv(daily_b2b_fname, header=0, sep=',')
-    else:    
-        B2BF=pd.DataFrame()
-        sub_fdir="sctg%s_truck/" %commoidty
-        for filename in glob.glob(fdir+sub_fdir+'*.csv'):
-            temp= pd.read_csv(filename, header=0, sep=',')
-            temp=temp.astype({'BuyerID': 'int64',
-            "BuyerZone":"int64",
-            "BuyerNAICS":"string",
-            "SellerID":"int64",
-            "SellerZone":"int64",
-            "SellerNAICS":"string",
-            "TruckLoad": "float64",
-            "SCTG_Group": "int64",
-            "shipment_id":"int64",
-            "orig_FAFID":"int64",
-            "dest_FAFID":"int64",
-            "mode_choice":"string",
-            "probability":"float64",
-            "Distance":"float64",
-            "Travel_time":"float64" 
-            })
-            # Add SellerCounty and BuyerCounty for filering 
-            temp = temp.merge(CBGzone_df[['MESOZONE','County']], left_on="SellerZone", right_on='MESOZONE', how='left')
-            temp=temp.rename({'County':'SellerCounty'}, axis=1)
-            temp = temp.merge(CBGzone_df[['MESOZONE','County']], left_on="BuyerZone", right_on='MESOZONE', how='left')
-            temp=temp.rename({'County':'BuyerCounty'}, axis=1)
-            if sel_county != 9999:
-                if ship_direction == "out":
-                    temp= temp[temp['SellerCounty']==sel_county].reset_index(drop=True)
-                elif ship_direction == "in":
-                    temp= temp[(temp['BuyerCounty']==sel_county) & (temp['SellerCounty']!=sel_county)].reset_index(drop=True)
-                elif ship_direction == "all":
-                    temp= temp[(temp['BuyerCounty']==sel_county) | (temp['SellerCounty']==sel_county)].reset_index(drop=True)
-                    temp= temp[~temp['SellerCounty'].isin(county_wo_sel)].reset_index(drop=True)
-            temp['TruckLoad']=temp['TruckLoad']*2000
-            temp["D_truckload"]=0
-            temp["D_selection"]=0
-            temp["D_truckload"]=temp['TruckLoad']
-            #temp["D_truckload"]=temp['TruckLoad'].apply(lambda x: b2b_d_truckload(x, weight_theshold))
-            temp["D_selection"]=temp['TruckLoad'].apply(lambda x: b2b_d_select_with_ship_size(x, weight_theshold, b2b_day_factor))
-            #temp["D_selection"]=temp['TruckLoad'].apply(lambda x: b2b_d_select(x, weight_theshold))
-            temp=temp.query('D_selection ==1')
-            B2BF=pd.concat([B2BF,temp],ignore_index=True)
-        B2BF.to_csv(fdir+'Daily_sctg%s_OD_%s_%s.csv' % (commoidty, sel_county,ship_direction), index = False, header=True)
+    # if file_exists(daily_b2b_fname):
+    #     B2BF=pd.read_csv(daily_b2b_fname, header=0, sep=',')
+    # else:    
+    B2BF=pd.DataFrame()
+    sub_fdir="sctg%s_truck/" %commoidty
+    for filename in glob.glob(fdir+sub_fdir+'*.csv'):
+        temp= pd.read_csv(filename, header=0, sep=',')
+        temp=temp.astype({'BuyerID': 'int64',
+        "BuyerZone":"int64",
+        "BuyerNAICS":"string",
+        "SellerID":"int64",
+        "SellerZone":"int64",
+        "SellerNAICS":"string",
+        "TruckLoad": "float64",
+        "SCTG_Group": "int64",
+        "shipment_id":"int64",
+        "orig_FAFID":"int64",
+        "dest_FAFID":"int64",
+        "mode_choice":"string",
+        "probability":"float64",
+        "Distance":"float64",
+        "Travel_time":"float64" 
+        })
+        # Add SellerCounty and BuyerCounty for filering 
+        temp = temp.merge(CBGzone_df[['MESOZONE','County']], left_on="SellerZone", right_on='MESOZONE', how='left')
+        temp=temp.rename({'County':'SellerCounty'}, axis=1)
+        temp = temp.merge(CBGzone_df[['MESOZONE','County']], left_on="BuyerZone", right_on='MESOZONE', how='left')
+        temp=temp.rename({'County':'BuyerCounty'}, axis=1)
+        if sel_county != 9999:
+            if ship_direction == "out":
+                temp= temp[temp['SellerCounty']==sel_county].reset_index(drop=True)
+            elif ship_direction == "in":
+                temp= temp[(temp['BuyerCounty']==sel_county) & (temp['SellerCounty']!=sel_county)].reset_index(drop=True)
+            elif ship_direction == "all":
+                temp= temp[(temp['BuyerCounty']==sel_county) | (temp['SellerCounty']==sel_county)].reset_index(drop=True)
+                temp= temp[~temp['SellerCounty'].isin(county_wo_sel)].reset_index(drop=True)
+        temp['TruckLoad']=temp['TruckLoad']*2000
+        temp["D_truckload"]=0
+        temp["D_selection"]=0
+        temp["D_truckload"]=temp['TruckLoad']
+        #temp["D_truckload"]=temp['TruckLoad'].apply(lambda x: b2b_d_truckload(x, weight_theshold))
+        temp["D_selection"]=temp['TruckLoad'].apply(lambda x: b2b_d_select_with_ship_size(x, weight_theshold, b2b_day_factor))
+        #temp["D_selection"]=temp['TruckLoad'].apply(lambda x: b2b_d_select(x, weight_theshold))
+        temp=temp.query('D_selection ==1')
+        B2BF=pd.concat([B2BF,temp],ignore_index=True)
+    B2BF.to_csv(fdir+'Daily_sctg%s_OD_%s_%s.csv' % (commoidty, sel_county,ship_direction), index = False, header=True)
     return B2BF
 
 def b2b_veh_type_truckload_prior(SCTG_Group,Distance, D_truckload, df_vius,dic_firm_stock):
